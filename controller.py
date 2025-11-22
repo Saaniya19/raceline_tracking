51439f7d342768aa264d7ef2182070da7f872dc6
Saaniya19 <saaniya.saraf@gmail.com>
2025-11-22 16:37:54 -0500

    comment

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ from numpy.typing import ArrayLike
 
 from simulator import RaceTrack
 
+# reach a constant desired speed with one controller (C1)
 def lower_controller(
     state : ArrayLike, desired : ArrayLike, parameters : ArrayLike
 ) -> ArrayLike:
@@ -11,6 +12,7 @@ def lower_controller(
 
     return np.array([0, 100]).T
 
+# fix steering angle to stay on the reference line with the other controller (C2)
 def controller(
     state : ArrayLike, parameters : ArrayLike, racetrack : RaceTrack
 ) -> ArrayLike:
